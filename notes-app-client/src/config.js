9436a1439f020259870a1cb4d7c3c17478e922ba
Srinivas Gurajala <srinivas.gurajala@gmail.com>
2018-08-16 16:38:53 +0530

    config file update

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -1,16 +1,16 @@
 export default {
   s3: {
-    REGION: "us_east_1",
-    BUCKET: "fs-notes-apps-upload"
+    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "us_east_1",
-    URL: "https://intrx2uq1g.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "YOUR_API_GATEWAY_REGION",
+    URL: "YOUR_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: "us_east_1",
-    USER_POOL_ID: "us-east-1_Rs2NffCdb",
-    APP_CLIENT_ID: "6gsh8fm66hd40mrrpgbn8e7i6o",
-    IDENTITY_POOL_ID: "us-east-1:9af1519d-0ff9-47b6-908d-474a22439efa"
+    REGION: "YOUR_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
   }
 };
